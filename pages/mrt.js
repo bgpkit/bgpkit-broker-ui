2c926b2bff5fe03f13b6a00b08a1168b91ff79c3
Mingwei Zhang <mingwei@mwzhang.com>
2023-04-18 20:34:30 -0700

    update data format

--- pages/mrt.js
+++ pages/mrt.js
@@ -41,7 +41,7 @@ function LatestMRT() {
         <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
     </svg>
 
-    let items = data['latest'];
+    let items = data['latest'].data;
     items = items.sort((a,b)=>{return a.collector_id > b.collector_id});
 
     for (let item of items) {
