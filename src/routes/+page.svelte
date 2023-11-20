13f15b86d00718b018fd2608c8b9ba56766b83f8
Mingwei Zhang <mingwei@mwzhang.com>
2023-11-19 17:38:36 -0800

    add local time and move data type to front

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,28 @@
 	const WARNING_ICON =`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" /> </svg>`;
 	const TRASH_ICON =`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /> </svg> `;
 
+	function toLocalTime(timeString) {
+		const now = new Date(timeString);
+
+		const offset = -now.getTimezoneOffset();
+		const offsetSign = offset >= 0 ? '+' : '-';
+		const offsetHours = Math.abs(Math.floor(offset / 60)).toString().padStart(2, '0');
+		const timeZone = `${offsetSign}${offsetHours}:00`;
+
+		const dateTimeFormat = new Intl.DateTimeFormat('en-US');
+
+		const year = now.getFullYear();
+		const month = (now.getMonth() + 1).toString().padStart(2, '0');
+		const day = now.getDate().toString().padStart(2, '0');
+		const hour = now.getHours().toString().padStart(2, '0');
+		const minute = now.getMinutes().toString().padStart(2, '0');
+		const second = now.getSeconds().toString().padStart(2, '0');
+
+		const dateTimeString = `${year}-${month}-${day}T${hour}:${minute}:${second}${timeZone}`;
+
+		return dateTimeString;
+	}
+
 	export const DEPRECATED_COLLECTORS = [
 		"rrc02",
 		"rrc08",
@@ -63,15 +85,16 @@
 		entry.ts_start = entry.ts_start.replace('T', ' ');
 		entry.rough_size = filesize(entry.rough_size).human('si');
 		entry.delay = `${duration(delay, 'seconds').humanize()} ago`;
+		entry.data_type = entry.data_type.toUpperCase();
 	}
 
 	let ontimePercentage = ((sourceData.length - delayed.length) / sourceData.length * 100).toFixed(1);
 
 	const tableSimple = {
 		// A list of heading labels.
-		head: ['Collector ID', 'Status', 'File Time UTC', 'Last Updated At', 'Type', 'Size',  'Download'],
+		head: ['Collector ID', 'Type', 'Status', 'File Time UTC', 'Last Updated At', 'Size',  'Latest'],
 		// The data visibly shown in your table body UI.
-		body: tableMapperValues(sourceData, ['collector_id', 'status', 'ts_start', 'delay', 'data_type', 'rough_size',  'url']),
+		body: tableMapperValues(sourceData, ['collector_id', 'data_type', 'status', 'ts_start', 'delay', 'rough_size',  'url']),
 		// Optional: A list of footer labels.
 		foot: ['Total', `<code class="code">${sourceData.length}</code> collectors`]
 	};
@@ -88,7 +111,10 @@
 		</a>
 		<div>
 			<h1 class="h1"> BGPKIT Broker Status </h1>
-			<h2 class="h3"> Last updated: {duration(diff/1000, 'seconds').humanize()} ago <span class="h6">({data.meta.latest_update_ts+'Z'})</span> </h2>
+			<h2 class="h3"> Last updated: {duration(diff/1000, 'seconds').humanize()} ago
+				<span class="h6">( GMT {data.meta.latest_update_ts+'Z'} )</span>
+				<span class="h6">( Local {toLocalTime(data.meta.latest_update_ts+'Z')} )</span>
+			</h2>
 		</div>
 		<div class="ml-auto">
 			<LightSwitch/>
