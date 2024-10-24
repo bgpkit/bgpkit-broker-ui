d1c4de41e5125a051c537df5118a06adc48e336a
Mingwei Zhang <mingwei@mwzhang.com>
2024-10-23 20:40:56 -0700

    reset to svelte5 and daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
-<script lang='ts'>
-	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
-    import { AppShell } from '@skeletonlabs/skeleton';
+<script>
+    import "../app.css";
 </script>
 
-<AppShell slotSidebarLeft="bg-gray-100">
-    <slot />
-</AppShell>
+<slot />
--- src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { BrokerData } from '../lib/types'; // Adjust the import path as needed
-
-/** @type {import('./$types').PageLoad} */
-export async function load(): Promise<BrokerData> {
-    const res = await fetch("https://api.bgpkit.com/v3/broker/latest");
-    const data: BrokerData = await res.json();
-    return data;
-}
-
